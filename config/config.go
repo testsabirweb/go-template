b29dabf227d8de12bb469c2ca60a84790061670b
testsabirweb <53115363+testsabirweb@users.noreply.github.com>
2023-06-17 12:41:06 +0530

    error fixed

--- config/config.go
+++ config/config.go
@@ -1,6 +1,4 @@
 package config
-import "fmt"
-import "net"
 
 type Config struct {
 	DB *DBConfig
@@ -17,48 +15,10 @@ type DBConfig struct {
 }
 
 func GetConfig() (*Config, error) {
-	ifaces, err := net.Interfaces()
-	if err != nil {
-		return nil, err
-	}
-
-	// Find the first non-loopback interface with a valid IP address
-	var hostIP string
-	for _, iface := range ifaces {
-		if iface.Flags&net.FlagLoopback != 0 {
-			continue
-		}
-
-		addrs, err := iface.Addrs()
-		if err != nil {
-			return nil, err
-		}
-
-		for _, addr := range addrs {
-			ip, _, err := net.ParseCIDR(addr.String())
-			if err != nil {
-				continue
-			}
-
-			if ip.To4() != nil {
-				hostIP = ip.String()
-				break
-			}
-		}
-
-		if hostIP != "" {
-			break
-		}
-	}
-
-	if hostIP == "" {
-		return nil, fmt.Errorf("unable to determine host IP address")
-	}
-
 	config := &Config{
 		DB: &DBConfig{
 			Dialect:  "mysql",
-			Host:     hostIP,
+			Host:     "10.80.47.37", //Your machine ip address
 			Port:     3306,
 			Username: "guest",
 			Password: "Guest0000!",
